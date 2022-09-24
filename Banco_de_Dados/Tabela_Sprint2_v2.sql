9c644743e808e473810c13fab4ee1e73f4f1c0a0
Rodrig0Lim4 <110926423+Rodrig0Lim4@users.noreply.github.com>
2022-09-24 17:47:24 -0300

    Add files via upload

---
+++ Banco_de_Dados/Tabela_Sprint2_v2.sql
@@ -0,0 +1,73 @@
+create database FungiTec;
+
+use FungiTec;
+
+create table Empresa (
+idEmpresa int primary key auto_increment,
+Nome varchar (45),
+Responsavel varchar (45),
+CNPJ char (14)
+);
+
+create table Contatos (
+idContatos int primary key auto_increment,
+Email varchar(45),
+TelefoneFixo char(8),
+TelefoneMovel char(11),
+fkEmpresa int,
+foreign key fkContatoEmpresa (fkEmpresa) references Empresa(idEmpresa)
+);
+
+create table DadosSensores (
+idDadosSensores int primary key auto_increment,
+Temperatura double,
+Umidade double,
+DiaHora datetime
+);
+
+create table CadastroCogumelos(
+idCogumelos int primary key auto_increment,
+especie varchar(45),
+fkDados int,
+foreign key fkDadosSensores (fkDados) references DadosSensores (idDadosSensores)
+);
+
+create table Estufas (
+idEstufa int primary key auto_increment,
+NomeEstufa varchar(45),
+fkCadastroCogumelo int,
+foreign key fkCadastro (fkCadastroCogumelo) references CadastroCogumelos (idCogumelos),
+fkCadastroEmpresa int,
+foreign key fkEmpresa (fkCadastroEmpresa) references Empresa (idEmpresa)
+);
+
+insert into Empresa (Nome, Responsavel, CNPJ) values
+	('SPtech','Fernando Brandão','07165496000100'),
+    ('FungiTec','Rafael Rodrigues','01234567891012'),
+    ('AgroXpto','Vivian Silva','11222344567891');
+
+insert into Contatos (email, TelefoneMovel, TelefoneFixo, fkEmpresa) values 
+('sptech.@gmail.com','11974093996','22045863', 1),
+('xpto@gmail.com','11987654321','22698135', 2),
+('school@gmail.com','11958467913','22647833', 3);
+
+insert into DadosSensores (Temperatura, Umidade, DiaHora) values
+    (20.70,90,'2022-09-06 14:30'),
+    (24.50,75,'2022-09-06 14:34'),
+    (20.43,85,'2022-09-06 14:37');
+
+insert into CadastroCogumelos (especie, fkDados) values 
+	('Shiitake', 1),
+    ('Shimeji', 2),
+    ('Champignon', 3);
+
+insert into Estufas (NomeEstufa, fkCadastroCogumelo, fkCadastroEmpresa) values
+('EstufaShitake', 1, 1),
+('EstufaShimeji', 2, 2),
+('EstufaChampignon', 3, 3);
+
+select * from Empresa;
+select * from Contatos;
+select * from CadastroCogumelos;
+select * from DadosSensores;
+select * from Estufas;
