2f7a553bf7cdb0be205c9c56b71c1fba45983080
Rodrig0Lim4 <rodrigo.oliveira@sptech.school>
2022-10-22 16:26:18 -0300

    alterações na tela inicial

--- Site Institucional/Site Estático/js/cadastro.js
+++ Site Institucional/Site Estático/js/cadastro.js
@@ -6,30 +6,30 @@ function validar_campo_cadastro(){
     var cpf = input_cpf.value;
 
 
-    if (nome_usuario.value == null) {
+    if (nome_usuario == null) {
         alert('Preencha este campo corretamente');
         nome.style.borderColor = '#ff0000';
 
-    }else if (email_usuario.value == null) {
+    }else if (email_usuario == null) {
         alert('Preencha este campo corretamente');
         email.style.borderColor = '#ff0000';
         
-    }else if (senha_usuario.value == null) {
+    }else if (senha_usuario == null) {
         alert('Por favor, cadastre uma senha!');
 
     }else if (senha_usuario.length < 8) {
         alert('Sua senha precisa ter  pelo menos oito caracteres');
         senha.style.borderColor = '#ff0000';
 
-    }else if (repeteir_senha.value == null) {
+    }else if (repeteir_senha == null) {
         alert('Insira sua senha novamente');
         Input_repetir_senha.style.borderColor = '#ff0000';
 
-    }else if (_senha.value != repeteir_senha.value) {
+    }else if (senha_usuario != repeteir_senha) {
         alert('Os campos não coincidem');
         input_repetir_senha.style.borderColor = '#ff0000';
 
-    }else if (cpf.value  == null) {
+    }else if (cpf == null) {
         alert('Campo inválido');
         input_cpf.style.borderColor = '#ff0000';
 
