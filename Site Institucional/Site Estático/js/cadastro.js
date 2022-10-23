509d2cf7354e8a2c212125540494f1c213ee2c2b
GuilhermeNPCosta <guilherme.costa@sptech.school>
2022-10-23 18:47:54 -0300

    Alteração nos scripts de validação e correção nas páginas do site

--- Site Institucional/Site Estático/js/cadastro.js
+++ Site Institucional/Site Estático/js/cadastro.js
@@ -2,11 +2,21 @@ function validar_campo_cadastro(){
     var nome_usuario = input_nome.value
     var email_usuario = input_email.value
     var senha_usuario = input_senha.value
-    var repeteir_senha = input_repetir_senha.value
+    var repetir_senha = input_repetir_senha.value
     var cpf = Number(input_cpf.value)
 
 
-    if (nome_usuario == "") {
+    if (nome_usuario == "" && email_usuario == ""  && senha_usuario == "" && repetir_senha.length == 0 && cpf == "") {
+        input_nome.style.borderColor = 'rgb(255, 0, 0)';
+        input_email.style.borderColor = 'rgb(255, 0, 0)';
+        input_senha.style.borderColor = 'rgb(255, 0, 0)';
+        input_repetir_senha.style.borderColor = 'rgb(255, 0, 0)';
+        input_cpf.style.borderColor = 'rgb(255, 0, 0)';
+
+        alert('Por favor, preeecha os campos abaixo.')
+    }
+
+    else if (nome_usuario == "") {
         input_nome.style.borderColor = 'rgb(255, 0, 0)';
         alert('Preencha o nome o corretamente');
 
@@ -22,11 +32,11 @@ function validar_campo_cadastro(){
         input_senha.style.borderColor = 'rgb(255, 0, 0)';
         alert('Sua senha precisa ter  pelo menos oito caracteres');
 
-    }else if (repeteir_senha.length == 0) {
-        Input_repetir_senha.style.borderColor = 'rgb(255, 0, 0)';
+    }else if (repetir_senha.length == 0) {
+        input_repetir_senha.style.borderColor = 'rgb(255, 0, 0)';
         alert('Insira sua senha novamente');
 
-    }else if (senha_usuario != repeteir_senha) {
+    }else if (senha_usuario != repetir_senha) {
         input_repetir_senha.style.borderColor = 'rgb(255, 0, 0)';
         alert('Os campos não coincidem');
 
@@ -34,14 +44,17 @@ function validar_campo_cadastro(){
         input_cpf.style.borderColor = 'rgb(255, 0, 0)';
         alert('Informe seu CPF para continuar');
     }
+
+
     else{
-        input_cpf.style.borderColor = null;
-        input_email.style.borderColor = null;
-        input_nome.style.borderColor = null;
-        input_senha.style.borderColor = null;
-        input_repetir_senha.style.borderColor = null;
+        input_cpf.style.borderColor = 'rgb (0, 255, 0)';
+        input_email.style.borderColor = 'rgb (0, 255, 0)';
+        input_nome.style.borderColor = 'rgb (0, 255, 0)';
+        input_senha.style.borderColor = 'rgb (0, 255, 0)';
+        input_repetir_senha.style.borderColor = 'rgb (0, 255, 0)';
         alert('Usuário Cadastrado com sucesso!');
+
     }
 
-}
+};
 
--- Site Institucional/Site Estático/js/login.js
+++ Site Institucional/Site Estático/js/login.js
@@ -1,23 +1,26 @@
 function validar_campo_login(){
-    var email_usuario = email.value;
+    var email_usuario = input_email.value;
     var senha_usuario = input_senha.value;
 
 
-     if (email_usuario.value == null) {
+     if (email_usuario.value == '') {
         alert('Preencha este campo corretamente');
-        email.style.borderColor = '#ff0000';
+        input_email.style.borderColor = '#ff0000';
     }
     
-    else if (senha_usuario.value == null) {
+    else if (senha_usuario.value == '') {
         alert('Por favor, cadastre uma senha!');
 
     }else if (senha_usuario.length < 8) {
         alert('Sua senha precisa ter  pelo menos oito caracteres');
-        senha.style.borderColor = '#ff0000';
+        input_senha.style.borderColor = '#ff0000';
 
     }
     else{
-        
+        input_email.style.borderColor = 'rgb (0, 255, 0)';
+        input_senha.style.borderColor = 'rgb (0, 255, 0)';
+
+        alert(`Bom dia! Bom te ver novamente!`)
     }
 
 };
