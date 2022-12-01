1b9a7db4003e21d723a4fe3000a6641a3b916b33
Nathan099-dev <tonininathan8@gmail.com>
2022-11-30 21:21:13 -0300

    começando o modal para formulário de contato

---
+++ Site Institucional/Site Estático/js/informatio.js
@@ -0,0 +1,10 @@
+var modal = {
+    open(){
+        document.getElementsByClassName('box-shadow').classList.add('visível');
+        
+    },
+
+    close(){
+        document.getElementsByClassName('box-shadow').classList.remove('visível');
+    }
+};
